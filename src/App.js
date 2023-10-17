17fd8359598c0bd8fa502ae9b1315b638c48a867
Daniel Møller Svensson <202007248@post.au.dk>
2023-10-17 15:30:17 +0200

    Changed sections to container component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Container from './components/container/Container';
 import ProjectCard from './components/projectCard/ProjectCard';
 import SkillTag from './components/skillTag/SkillTag';
 
@@ -14,8 +15,7 @@ function App() {
         <p>Welcome to my developer portfolio, where I showcase my expertise in frontend development &#128187;. With a passion for creating visually stunning and user-friendly websites I strive to deliver high-quality projects that exceed expectations</p>
         <a className='btn' href='#projects'>View Projects</a>
       </div>
-      <section id='projects' className='container'>
-        <h2>Projects</h2>
+      <Container id={'projects'} title={'Projects'}>
         <div className='projects'>
           <ProjectCard image={`${publicURL}/images/TDFOverview.png`}
             title={'Tour de France overview'}
@@ -23,36 +23,34 @@ function App() {
                           The idea is that the user can scroll through every stage to see who got different key points and who won the stage. 
                           After every stage, there is an overview of who is in yellow, green, and so on`}
             link={'https://dmsvensson.github.io/grandTours/'}
-            skills={[{skill:'React', type:'framework'}, {skill:'Javascript', type: 'language'}, {skill:'HTML, CSS', type: 'language'}]} />
+            skills={[{ skill: 'React', type: 'framework' }, { skill: 'Javascript', type: 'language' }, { skill: 'HTML, CSS', type: 'language' }]} />
           <ProjectCard image={`${publicURL}/images/treeman.png`}
             title={'Træmanden'}
             description={`I have designed and create a more modern version of Træmanden.dk`}
             link={'https://treeman-644ca.web.app/'}
-            skills={[{skill:'Angular', type: 'framework'}, {skill:'Typescript', type: 'language'}, {skill:'HTML, CSS', type: 'language'}, {skill: 'Figma', type:'other'}, {skill: 'Sanity', type: 'other'}]} />
+            skills={[{ skill: 'Angular', type: 'framework' }, { skill: 'Typescript', type: 'language' }, { skill: 'HTML, CSS', type: 'language' }, { skill: 'Figma', type: 'other' }, { skill: 'Sanity', type: 'other' }]} />
           <ProjectCard image={`${publicURL}/images/ginWebsite.png`}
             title={'Gin Tasting'}
             description={`I held a gin tasting at my university, where a fellow student and I created a website for this event.
                           The other students could go on the website to rate the gin in order to find the best gin.
                           To see an event use code: 7765`}
             link={'https://gin-tasting.web.app/'}
-            skills={[{skill:'Angular', type: 'framework'}, {skill:'Typescript', type: 'language'}, {skill:'Firebase', type: 'other'}]} />
+            skills={[{ skill: 'Angular', type: 'framework' }, { skill: 'Typescript', type: 'language' }, { skill: 'Firebase', type: 'other' }]} />
           <ProjectCard image={`${publicURL}/images/myFitLog.png`}
             title={'MyFitLog'}
             description={`MyFitLog is a website for tracking strength training. Demo user email: test@test.com password: 123456`}
-            skills={[{skill:'Angular', type: 'framework'}, {skill:'Typescript', type: 'language'}, {skill:'HTML, CSS', type: 'language'}, {skill:'Firebase', type: 'other'}]} />
+            skills={[{ skill: 'Angular', type: 'framework' }, { skill: 'Typescript', type: 'language' }, { skill: 'HTML, CSS', type: 'language' }, { skill: 'Firebase', type: 'other' }]} />
           <ProjectCard image={'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80'}
             title={'Smart Mirror'}
             description={`I created a smart mirror and an app. The idea is that my family can have useful information shown on the smart mirror.`}
-            skills={[{skill:'Ionic', type:'framework'}, {skill:'Firebase', type: 'other'}]} />
+            skills={[{ skill: 'Ionic', type: 'framework' }, { skill: 'Firebase', type: 'other' }]} />
           <ProjectCard image={'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80'}
             title={'TilbudsChecker'}
             description={`One of my first app. The idea is that you can take an URL of a product and add it to the app. The app will then check after discount on the products once a day.`}
-            skills={[{skill:'C#', type: 'language'}, {skill:'UWP', type: 'other'}]} />
+            skills={[{ skill: 'C#', type: 'language' }, { skill: 'UWP', type: 'other' }]} />
         </div>
-      </section>
-      <section className='about-section'>
-        <div className='container'>
-        <h2>About me</h2>
+      </Container>
+      <Container id={'about'} title={'About Me'} backgroundColor={'#302d2c'}>
         <div className='about'>
           <article className='about-me'>
             <p>My journey as a developer spans over the last 9 years, during which I have primarily focused on
@@ -92,14 +90,13 @@ function App() {
                 <SkillTag type={'other'} skill={'Git'} />
                 <SkillTag type={'other'} skill={'Firebase'} />
                 <SkillTag type={'other'} skill={'Figma'} />
-                <SkillTag type={'other'} skill={'Sanity'}/>
+                <SkillTag type={'other'} skill={'Sanity'} />
                 <SkillTag type={'other'} skill={'UWP'} />
               </div>
             </article>
           </article>
         </div>
-        </div>
-      </section>
+      </Container>
     </div>
   );
 }
